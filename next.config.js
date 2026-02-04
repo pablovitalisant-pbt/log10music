7243cb1ac2a5fad986062aaed98477ace2d639d0
pablovitalisant-pbt <pablovitalisant@gmail.com>
2026-02-04 01:45:10 -0300

    fix: remove invalid outputFileTracingRoot for vercel build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     swcPlugins: [],
   },
-  outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 module.exports = nextConfig;
